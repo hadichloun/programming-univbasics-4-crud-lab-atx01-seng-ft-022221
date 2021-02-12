2b5de208980591ec23f3e4b5391e679e8ac8bfa2
Hadi Chloun <hadi_960@hotmail.com>
2021-02-12 00:19:44 +0000

    Automatically backed up by Learn

--- spec/array_crud_spec.rb
+++ spec/array_crud_spec.rb
@@ -1,85 +1,74 @@
-  
-require './lib/intro_to_simple_array_manipulations.rb'
+require "spec_helper"
 
-
-describe "using_push" do 
-  it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
-    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
-    updated_array = using_push(colors_in_the_rainbow, next_color)
-    expect(updated_array.last).to eq(next_color)
-    end
-end
-
-describe "using_unshift" do 
-  before(:each) do 
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    @new_neighborhood = "Staten Island"
-    @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
-  end
-  
-  it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
-    expect(@updated_array.first).to eq(@new_neighborhood)
-  end
-
-  it "increases the length of the array" do 
-    expect(@updated_array.size).to eq(5)
-  end
-end
-
-describe "using_pop" do
-  before(:each) do 
-    @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-    @deleted_string = using_pop(@continents)
-  end
-  it "takes in argument of an array and uses the pop method to remove the last element from the array and return that element" do 
-    expect(@deleted_string).to eq("Antarctica")
-  end
-
-  it "decreases the length of the array by 1" do 
-    expect(@continents.size).to eq(6)
-  end
-end
-
-describe "pop_with_args" do
-  before(:each) do 
-    @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    @small_dogs = pop_with_args(@dog_breeds)
+describe "create_an_empty_array" do
+  it "creates and returns and empty array" do 
+    expect(create_an_empty_array).to eq([])
   end 
-  it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
-    expect(@small_dogs).to eq(["Chihuahua", "Shiba Inu"])
-  end 
+end
 
-  it "decreases the length of the array by 2" do
-    expect(@dog_breeds.size).to eq(2)
+describe "create_an_array" do 
+  it "creates an array with four elements" do 
+    expect(create_an_array).to be_a(Array)
+    expect(create_an_array.size).to eq(4)
   end
 end
 
-describe "using_shift" do 
-  before(:each) do 
-    @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-    @im_so_over_this_city = using_shift(@my_favorite_cities)
-  end
-  it "takes in an argument of an array and uses the shift method to remove the first item and return it" do 
-    expect(@im_so_over_this_city).to eq("Lagos")
-  end
-
-  it "decreases the length of the array by 1" do 
-    expect(@my_favorite_cities.size).to eq(7)
+describe "add_element_to_end_of_array" do 
+  it "takes arguments of an array and an element and adds that element to the end of the array" do 
+    array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    expect(add_element_to_end_of_array(array, element).last).to eq("arrays!")
   end
 end
 
-describe "shift_with_args" do 
-  before(:each) do 
-    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    @brands_removed = shift_with_args(@ice_cream_brands)
+describe "add_element_to_start_of_array" do 
+  it "takes arguments of an array and an element and adds that element to the start of the array" do 
+    array = ["I", "am", "really", "learning"]
+    element = "wow"
+    expect(add_element_to_start_of_array(array, element).first).to eq("wow")
   end
-  
-  it "takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array" do 
-    expect(@brands_removed).to eq(["Blue Bell Creameries", "Ben & Jerry's"])
-  end
+end
 
-  it "decreases the length of the array by 2" do 
-    expect(@brands_removed.size).to eq(2)
+describe "remove_element_from_end_of_array" do 
+  it "takes in an argument of an array and removes the last element in the array" do 
+    array = ["I", "am", "really", "learning", "arrays!"]
+    expect(remove_element_from_end_of_array(array)).to eq("arrays!")
+  end
+end
+
+describe "remove_element_from_start_of_array" do 
+  it "takes in an argument of an array and removes the first element from the array" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(remove_element_from_start_of_array(array)).to eq("wow")
+  end
+end
+
+describe "retrieve_element_from_index" do 
+  it "takes in an argument of an array and an index number and returns the element stored at that index" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    index_number = 2
+    expect(retrieve_element_from_index(array, 2)).to eq("am")
+  end
+end
+
+describe "retrieve_first_element_from_array" do 
+  it "takes in an argument of an array and returns the first element stored in the array" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(retrieve_first_element_from_array(array)).to eq("wow")
+  end
+end
+
+describe "retrieve_last_element_from_array" do 
+  it "takes in an argument of an array and returns the last element of that array" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(retrieve_last_element_from_array(array)).to eq("arrays!")
+  end
+end
+
+describe "update_element_from_index" do 
+  it "takes in an argument of an array, an index number and element value and returns the updated element" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    index_number = 4
+    expect(update_element_from_index(array, 4, "totally")).to eq("totally")
   end
 end
